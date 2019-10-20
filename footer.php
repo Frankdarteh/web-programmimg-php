0195d9105e5c20052eb38e57ddd3754109765561
Frank Darteh <42451891+Frankdarteh@users.noreply.github.com>
2019-10-20 19:53:26 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,26 +1,27 @@
 <nav class="navbar navbar-inverse"id="footerlast" >
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="findbook.php">keeping books</a>
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="findbook.php">keeping books</a>
+        </div>
+        <div class="active">
+          <ul class="nav navbar-nav" >
+              <li ><a href="Libraryhome.php">Home</a></li>
+              <li><a href="https://www.facebook.com/dartey.frank">Facebook</a></li>
+              <li><a href="https://mail.google.com/mail/u/0/#inbox">Email</a></li>
+              <li><a href="https://www.instagram.com/dartehfrank/?hl=pl">Instagram</a></li>
+              <li><a href="#">Contacts</a></li>
+              <li>  
+                <form class="form-inline">
+                    <input type="email" class="form-control" size="50" placeholder="Email Address">
+                    <button type="button" class="btn btn-danger">Sign Up</button>
+                  </form>
+              </li>
+            </ul>
+      </div>
+      </div>
+    </nav>
+    <div class="container">
+      <h3>Fixt Boy</h3>
+      <p><h2>I An inverting you to IT word.</h2></p>
     </div>
-    <div class="active">
-       <ul class="nav navbar-nav" >
-           <li ><a href="Libraryhome.php">Home</a></li>
-           <li><a href="https://www.facebook.com/dartey.frank">Facebook</a></li>
-           <li><a href="https://mail.google.com/mail/u/0/#inbox">Email</a></li>
-           <li><a href="https://www.instagram.com/dartehfrank/?hl=pl">Instagram</a></li>
-           <li><a href="#">Contacts</a></li>
-           <li>  
-             <form class="form-inline">
-                <input type="email" class="form-control" size="50" placeholder="Email Address">
-                <button type="button" class="btn btn-danger">Sign Up</button>
-             </form>
-           </li>
-        </ul>
-   </div>
-</nav>
-<div class="container">
-  <h3>Fixt Boy</h3>
-  <p><h2>I An inverting you to IT word.</h2></p>
-</div>
-</html>
+    </html>
