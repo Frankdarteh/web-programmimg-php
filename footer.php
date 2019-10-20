c1f433c70e3353c4807101c2fe4862e4f96890bd
Frank Darteh <42451891+Frankdarteh@users.noreply.github.com>
2019-10-20 19:49:18 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
              <form class="form-inline">
                 <input type="email" class="form-control" size="50" placeholder="Email Address">
                 <button type="button" class="btn btn-danger">Sign Up</button>
-              </form>
+             </form>
            </li>
         </ul>
    </div>
